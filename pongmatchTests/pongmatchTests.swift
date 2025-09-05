69b20be71fdbee6da6a385120ec4b8452329fd0b
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2025-09-05 19:54:13 +0200

    Initial Commit

---
+++ pongmatchTests/pongmatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  pongmatchTests.swift
+//  pongmatchTests
+//
+//  Created by Jordi Puigdellívol on 5/9/25.
+//
+
+import Testing
+
+struct pongmatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
