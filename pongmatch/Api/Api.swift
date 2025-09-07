f272f226a812e59413489647707b855d05cf4fe6
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2025-09-07 10:23:58 +0200

    wip starting with api login

---
+++ pongmatch/Api/Api.swift
@@ -0,0 +1,16 @@
+import Foundation
+import RevoHttp
+
+
+class Api {
+    
+    let url = "http://pongmatch.test/api"
+
+    func token(_ email:String, password:String, device:String) async throws -> String {
+        
+        
+        
+    }
+    
+    
+}
