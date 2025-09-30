3ee8f364372b4d32d6cf410513df3bd3560efcb4
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2025-09-30 16:27:35 +0200

    Added recent players to search opponent and simplified ui

--- pongmatch/Views/Components/SearchOpponentView.swift
+++ pongmatch/Views/Components/SearchOpponentView.swift
@@ -6,51 +6,70 @@ struct SearchOpponentView : View {
     @EnvironmentObject private var auth: AuthViewModel
     
     @State private var searchText: String = ""
-    @State private var searchResults: [User] = [User.unknown()]
+    @State private var searchResults: [User] = []
+    @State var recentOpponents: [User] = []
     
     @Binding var selectedFriend: User
     var onSelected:(_ player:User) -> Void = {_ in }
     @FocusState private var isSearchFocused: Bool
-
+        
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading){
-                
-                Text("Select Opponent")
-                    .font(.title2)
-                    .bold()
-                    .padding(.bottom)
-
-                
-                VStack(spacing: 10) {
+            List {
+                Section {
                     ForEach(searchResults, id: \.id) { friend in
                         HStack{
                             UserView(user: friend)
                             Spacer()
                         }
+                        .contentShape(Rectangle())
                         .onTapGesture() {
                             onSelected(friend)
                         }
-                        Divider()
                     }
-                    Spacer()
                 }
-                Spacer()
+                if searchText.isEmpty {
+                    Section(header: Text("Recent")) {
+                        ForEach(recentOpponents, id: \.id) { friend in
+                            HStack{
+                                UserView(user: friend)
+                                Spacer()
+                            }
+                            .contentShape(Rectangle())
+                            .onTapGesture() {
+                                onSelected(friend)
+                            }
+                        }
+                    }
+                }
             }
-            .padding()
+            .navigationTitle("Search Opponent")
             .searchable(text: $searchText, prompt: "Search for friends")
             .focused($isSearchFocused)
             .onAppear { isSearchFocused = true }
             .onChange(of: searchText) { _, newValue in
-                Task {
-                    if newValue.isEmpty {
-                        searchResults = [User.unknown()]
-                    } else {
-                        try? await Task.sleep(nanoseconds: 500_000_000) // 0.5s delay
-                        searchResults = ((try? await auth.searchFriends(newValue)) ?? []) + [User.unknown()].filter {
-                            $0.canBeChallengedByMe()
-                        }
-                    }
+                search(newValue)
+            }
+        }
+        .task {
+            recentOpponents = auth.games.filter {
+                $0.isFinished()
+            }.map {
+                $0.player1.id == auth.user?.id ? $0.player2 : $0.player1
+            }.unique(\.id).filter{
+                $0.canBeChallengedByMe()
+            } + [User.unknown()]
+        }
+    }
+    
+    private func search(_ text:String){
+        Task {
+            if text.isEmpty {
+                searchResults = [User.unknown()]
+            } else {
+                try? await Task.sleep(nanoseconds: 500_000_000) // 0.5s delay
+                searchResults = ((try? await auth.searchFriends(text)) ?? []).filter {
+                    $0.canBeChallengedByMe()
                 }
             }
         }
