4ae4f8dd02cd42c7be42bcc9f4cad4ec492bd48d
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2025-09-30 16:09:55 +0200

    Improved watch selection view

--- pongmatch-watch Watch App/Views/ScoreboardSelectionView.swift
+++ pongmatch-watch Watch App/Views/ScoreboardSelectionView.swift
@@ -7,39 +7,46 @@ struct ScoreboardSelectionView : View {
     @State private var winningCondition:WinningCondition = .bestof3
     
     var body: some View {
-        
-        Picker("Initial Score", selection: $initialScore) {
-            ForEach(InitialScore.allCases, id:\.self) { initialScore in
-                Text(initialScore.rawValue.capitalized)
+        ScrollView{
+            VStack {
+                HStack{
+                    Picker("Initial Score", selection: $initialScore) {
+                        ForEach(InitialScore.allCases, id:\.self) { initialScore in
+                            Text(initialScore.rawValue.capitalized)
+                        }
+                    }
+                    
+                    Picker("Win condition", selection: $winningCondition) {
+                        ForEach(WinningCondition.allCases, id:\.self) { condition in
+                            Text(condition.rawValue.capitalized)
+                        }
+                    }
+                }.frame(height:60)
+                
+                HStack{
+                    Picker("Ranking Type", selection: $rankingType) {
+                        ForEach(RankingType.allCases, id:\.self) { rankingType in
+                            Text(rankingType.rawValue.capitalized)
+                        }
+                    }
+                }.frame(height:60)
+                
+                NavigationLink("Start") {
+                    ScoreboardView(score: Score(game:
+                        Game(
+                            id: -1,
+                            initial_score: initialScore,
+                            ranking_type: rankingType,
+                            winning_condition: winningCondition,
+                            information: nil,
+                            date: Date(),
+                            status: .ongoing,
+                            player1: IPhoneSync.authUser() ?? User.unknown(),
+                            player2: User.unknown()
+                        )
+                   ))
+                }.padding(.vertical, 20)
             }
-        }
-        
-        Picker("Ranking Type", selection: $rankingType) {
-            ForEach(RankingType.allCases, id:\.self) { rankingType in
-                Text(rankingType.rawValue.capitalized)
-            }
-        }
-        
-        Picker("Win condition", selection: $winningCondition) {
-            ForEach(WinningCondition.allCases, id:\.self) { condition in
-                Text(condition.rawValue.capitalized)
-            }
-        }
-        
-        NavigationLink("Start") {
-            ScoreboardView(score: Score(game:
-                Game(
-                    id: -1,
-                    initial_score: initialScore,
-                    ranking_type: rankingType,
-                    winning_condition: winningCondition,
-                    information: nil,
-                    date: Date(),
-                    status: .ongoing,
-                    player1: IPhoneSync.authUser() ?? User.unknown(),
-                    player2: User.unknown()
-                )
-            ))
         }
     }
     
